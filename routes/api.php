d2b7511f473fce2bea69d77c180b7544f7c408ed
User Name <Email@example.com>
2023-07-31 15:23:21 +0200

    "updates"

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,26 @@
 <?php
 
+use App\Http\Controllers\Api\AdvertisementController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CountryController;
+use App\Http\Controllers\Api\FaqController;
+use App\Http\Controllers\Api\FeatureController;
+use App\Http\Controllers\Api\FollowUpPagesController;
 use App\Http\Controllers\Api\ForgotPasswordController;
+use App\Http\Controllers\Api\LiveController;
+use App\Http\Controllers\Api\LiveFacebookController;
+use App\Http\Controllers\Api\LiveInstagramController;
+use App\Http\Controllers\Api\LiveLinkedinController;
+use App\Http\Controllers\Api\LiveTwitterController;
+use App\Http\Controllers\Api\LiveYoutubeController;
+use App\Http\Controllers\Api\MagazineController;
+use App\Http\Controllers\Api\NotificationController;
+use App\Http\Controllers\Api\PerformanceController;
+use App\Http\Controllers\Api\RecentAchievementController;
+use App\Http\Controllers\Api\SubscriptionController;
+use App\Http\Controllers\Api\TeamController;
+use App\Http\Controllers\Api\TopNotificationController;
+use App\Http\Controllers\Api\TrainningVideoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Http;
@@ -46,55 +64,55 @@ Route::group(['middleware' => ['api','isAdmin']], function ($router) {
 
 
     Route::group(['middleware' => ['api'], 'prefix' => 'team'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\TeamController@index');
-        Route::post('save', 'App\Http\Controllers\Api\TeamController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\TeamController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\TeamController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\TeamController@destroy');
+        Route::get('show_all', [TeamController::class,'index']);
+        Route::post('save', [TeamController::class,'store']);
+        Route::post('show/{id}', [TeamController::class,'show']);
+        Route::post('update/{id}', [TeamController::class,'update']);
+        Route::post('delete/{id}', [TeamController::class,'destroy']);
     });
 
     Route::group(['middleware' => ['api'], 'prefix' => 'magazine'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\MagazineController@index');
-        Route::post('save', 'App\Http\Controllers\Api\MagazineController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\MagazineController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\MagazineController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\MagazineController@destroy');
+        Route::get('show_all', [MagazineController::class,'index']);
+        Route::post('save', [MagazineController::class,'store']);
+        Route::post('show/{id}', [MagazineController::class,'show']);
+        Route::post('update/{id}', [MagazineController::class,'update']);
+        Route::post('delete/{id}', [MagazineController::class,'destroy']);
     });
 
     Route::group(['middleware' => ['api'], 'prefix' => 'faq'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\FaqController@index');
-        Route::post('save', 'App\Http\Controllers\Api\FaqController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\FaqController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\FaqController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\FaqController@destroy');
+        Route::get('show_all', [FaqController::class,'index']);
+        Route::post('save', [FaqController::class,'store']);
+        Route::post('show/{id}', [FaqController::class,'show']);
+        Route::post('update/{id}', [FaqController::class,'update']);
+        Route::post('delete/{id}', [FaqController::class,'destroy']);
     });
     Route::group(['middleware' => ['api'], 'prefix' => 'performance'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\PerformanceController@index');
-        Route::post('save', 'App\Http\Controllers\Api\PerformanceController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\PerformanceController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\PerformanceController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\PerformanceController@destroy');
+        Route::get('show_all', [PerformanceController::class,'index']);
+        Route::post('save', [PerformanceController::class,'store']);
+        Route::post('show/{id}', [PerformanceController::class,'show']);
+        Route::post('update/{id}', [PerformanceController::class,'update']);
+        Route::post('delete/{id}', [PerformanceController::class,'destroy']);
     });
     Route::group(['middleware' => ['api'], 'prefix' => 'advertisement'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\AdvertisementController@index');
-        Route::post('save', 'App\Http\Controllers\Api\AdvertisementController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\AdvertisementController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\AdvertisementController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\AdvertisementController@destroy');
+        Route::get('show_all', [AdvertisementController::class,'index']);
+        Route::post('save', [AdvertisementController::class,'store']);
+        Route::post('show/{id}', [AdvertisementController::class,'show']);
+        Route::post('update/{id}', [AdvertisementController::class,'update']);
+        Route::post('delete/{id}', [AdvertisementController::class,'destroy']);
     });
     Route::group(['middleware' => ['api'], 'prefix' => 'live'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\LiveController@index');
-        Route::post('save', 'App\Http\Controllers\Api\LiveController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\LiveController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\LiveController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\LiveController@destroy');
+        Route::get('show_all', [LiveController::class,'index']);
+        Route::post('save', [LiveController::class,'store']);
+        Route::post('show/{id}', [LiveController::class,'show']);
+        Route::post('update/{id}', [LiveController::class,'update']);
+        Route::post('delete/{id}', [LiveController::class,'destroy']);
     });
     Route::group(['middleware' => ['api'], 'prefix' => 'notification'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\NotificationController@index');
-        Route::post('save', 'App\Http\Controllers\Api\NotificationController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\NotificationController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\NotificationController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\NotificationController@destroy');
+        Route::get('show_all', [NotificationController::class,'index']);
+        Route::post('save', [NotificationController::class,'store']);
+        Route::post('show/{id}', [NotificationController::class,'show']);
+        Route::post('update/{id}', [NotificationController::class,'update']);
+        Route::post('delete/{id}', [NotificationController::class,'destroy']);
     });
 
     Route::group(['middleware' => ['api'], 'prefix' => 'plan'], function ($router) {
@@ -102,6 +120,10 @@ Route::group(['middleware' => ['api','isAdmin']], function ($router) {
         Route::post('savePlan', 'App\Http\Controllers\Api\SubscriptionController@savePlan');
         Route::post('checkoutPlan', 'App\Http\Controllers\Api\SubscriptionController@checkout');
         Route::get('allSubscriptions', 'App\Http\Controllers\Api\SubscriptionController@allSubscriptions');
+        Route::get('show_all', [SubscriptionController::class,'showPlans']);
+        Route::post('savePlan', [SubscriptionController::class,'savePlan']);
+        Route::post('checkoutPlan', [SubscriptionController::class,'checkout']);
+        Route::post('allSubscriptions', [SubscriptionController::class,'allSubscriptions']);
     });
 
 //    Route::group(['middleware' => ['api'], 'prefix' => 'subscription'], function ($router) {
@@ -113,86 +135,86 @@ Route::group(['middleware' => ['api','isAdmin']], function ($router) {
 //    });
 
     Route::group(['middleware' => ['api'], 'prefix' => 'topnotification'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\TopNotificationController@index');
-        Route::post('save', 'App\Http\Controllers\Api\TopNotificationController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\TopNotificationController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\TopNotificationController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\TopNotificationController@destroy');
+        Route::get('show_all', [TopNotificationController::class,'index']);
+        Route::post('save', [TopNotificationController::class,'store']);
+        Route::post('show/{id}', [TopNotificationController::class,'show']);
+        Route::post('update/{id}', [TopNotificationController::class,'update']);
+        Route::post('delete/{id}', [TopNotificationController::class,'destroy']);
     });
 
     Route::group(['middleware' => ['api'], 'prefix' => 'recentachievement'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\RecentAchievementController@index');
-        Route::post('save', 'App\Http\Controllers\Api\RecentAchievementController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\RecentAchievementController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\RecentAchievementController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\RecentAchievementController@destroy');
+        Route::get('show_all', [RecentAchievementController::class,'index']);
+        Route::post('save', [RecentAchievementController::class,'store']);
+        Route::post('show/{id}', [RecentAchievementController::class,'show']);
+        Route::post('update/{id}', [RecentAchievementController::class,'update']);
+        Route::post('delete/{id}', [RecentAchievementController::class,'destroy']);
     });
 
     Route::group(['middleware' => ['api'], 'prefix' => 'feature'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\FeatureController@index');
-        Route::post('save', 'App\Http\Controllers\Api\FeatureController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\FeatureController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\FeatureController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\FeatureController@destroy');
+        Route::get('show_all', [FeatureController::class,'index']);
+        Route::post('save', [FeatureController::class,'store']);
+        Route::post('show/{id}', [FeatureController::class,'show']);
+        Route::post('update/{id}', [FeatureController::class,'update']);
+        Route::post('delete/{id}', [FeatureController::class,'destroy']);
     });
 
     Route::group(['middleware' => ['api'], 'prefix' => 'FollowUpPages'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\FollowUpPagesController@index');
-        Route::post('save', 'App\Http\Controllers\Api\FollowUpPagesController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\FollowUpPagesController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\FollowUpPagesController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\FollowUpPagesController@destroy');
+        Route::get('show_all', [FollowUpPagesController::class,'index']);
+        Route::post('save', [FollowUpPagesController::class,'store']);
+        Route::post('show/{id}', [FollowUpPagesController::class,'show']);
+        Route::post('update/{id}', [FollowUpPagesController::class,'update']);
+        Route::post('delete/{id}', [FollowUpPagesController::class,'destroy']);
     });
 
     Route::group(['middleware' => ['api'], 'prefix' => 'training_video'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\TrainningVideoController@index');
-        Route::post('save', 'App\Http\Controllers\Api\TrainningVideoController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\TrainningVideoController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\TrainningVideoController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\TrainningVideoController@destroy');
+        Route::get('show_all', [TrainningVideoController::class,'index']);
+        Route::post('save', [TrainningVideoController::class,'store']);
+        Route::post('show/{id}', [TrainningVideoController::class,'show']);
+        Route::post('update/{id}', [TrainningVideoController::class,'update']);
+        Route::post('delete/{id}', [TrainningVideoController::class,'destroy']);
     });
 
 
     Route::group(['middleware' => ['api'], 'prefix' => 'livefacebook'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\LiveFaceBookController@index');
-        Route::post('save', 'App\Http\Controllers\Api\LiveFaceBookController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\LiveFaceBookController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\LiveFaceBookController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\LiveFaceBookController@destroy');
+        Route::get('show_all', [LiveFaceBookController::class,'index']);
+        Route::post('save', [LiveFaceBookController::class,'store']);
+        Route::post('show/{id}', [LiveFaceBookController::class,'show']);
+        Route::post('update/{id}', [LiveFaceBookController::class,'update']);
+        Route::post('delete/{id}', [LiveFaceBookController::class,'destroy']);
     });
 
 
     Route::group(['middleware' => ['api'], 'prefix' => 'livetwitter'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\LiveTwitterController@index');
-        Route::post('save', 'App\Http\Controllers\Api\LiveTwitterController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\LiveTwitterController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\LiveTwitterController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\LiveTwitterController@destroy');
+        Route::get('show_all', [LiveTwitterController::class,'index']);
+        Route::post('save', [LiveTwitterController::class,'store']);
+        Route::post('show/{id}', [LiveTwitterController::class,'show']);
+        Route::post('update/{id}', [LiveTwitterController::class,'update']);
+        Route::post('delete/{id}', [LiveTwitterController::class,'destroy']);
     });
 
     Route::group(['middleware' => ['api'], 'prefix' => 'liveyoutube'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\LiveYoutubeController@index');
-        Route::post('save', 'App\Http\Controllers\Api\LiveYoutubeController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\LiveYoutubeController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\LiveYoutubeController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\LiveYoutubeController@destroy');
+        Route::get('show_all', [LiveYoutubeController::class,'index']);
+        Route::post('save', [LiveYoutubeController::class,'store']);
+        Route::post('show/{id}', [LiveYoutubeController::class,'show']);
+        Route::post('update/{id}', [LiveYoutubeController::class,'update']);
+        Route::post('delete/{id}', [LiveYoutubeController::class,'destroy']);
     });
 
     Route::group(['middleware' => ['api'], 'prefix' => 'liveinstagram'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\LiveInstagramController@index');
-        Route::post('save', 'App\Http\Controllers\Api\LiveInstagramController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\LiveInstagramController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\LiveInstagramController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\LiveInstagramController@destroy');
+        Route::get('show_all', [LiveInstagramController::class,'index']);
+        Route::post('save', [LiveInstagramController::class,'store']);
+        Route::post('show/{id}', [LiveInstagramController::class,'show']);
+        Route::post('update/{id}', [LiveInstagramController::class,'update']);
+        Route::post('delete/{id}', [LiveInstagramController::class,'destroy']);
     });
 
 
     Route::group(['middleware' => ['api'], 'prefix' => 'livelinkedin'], function ($router) {
-        Route::get('show_all', 'App\Http\Controllers\Api\LivelinkedinController@index');
-        Route::post('save', 'App\Http\Controllers\Api\LivelinkedinController@store');
-        Route::post('show/{id}', 'App\Http\Controllers\Api\LivelinkedinController@show');
-        Route::post('update/{id}', 'App\Http\Controllers\Api\LivelinkedinController@update');
-        Route::post('delete/{id}', 'App\Http\Controllers\Api\LivelinkedinController@destroy');
+        Route::get('show_all', [LiveLinkedinController::class,'index']);
+        Route::post('save', [LiveLinkedinController::class,'store']);
+        Route::post('show/{id}', [LiveLinkedinController::class,'show']);
+        Route::post('update/{id}', [LiveLinkedinController::class,'update']);
+        Route::post('delete/{id}', [LiveLinkedinController::class,'destroy']);
     });
 
 
