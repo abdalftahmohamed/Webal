fffa46f863849a244fbd948d834c795becacc7e3
User Name <Email@example.com>
2023-08-15 18:40:22 +0200

    "updates"

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -151,11 +151,10 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth('api')->factory()->getTTL() * 60,
+//            'expires_in' => auth('api')->factory()->getTTL() * 60,
             'user' => auth('api')->user()
         ]);
     }
-
 
 
     public function destroy()
