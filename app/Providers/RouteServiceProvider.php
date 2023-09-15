2b78022ade6576e78d550ee0a60ba9f002004372
User Name <Email@example.com>
2023-09-15 03:40:41 +0300

    "updates now"

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -25,7 +25,8 @@ class RouteServiceProvider extends ServiceProvider
     public function boot(): void
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            $perMinute = env('APP_ENV') === 'testing' ? 1000 : 60;
+            return Limit::perMinute($perMinute)->by($request->user()?->id ?: $request->ip());
         });
 
         $this->routes(function () {
